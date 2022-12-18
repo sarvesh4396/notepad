e62349de179356da9188756a869d4f22dde75eda
sarvesh4396 <heysarvesh@pm.me>
2022-12-18 18:54:37 +0530

    add note model

---
+++ lib/models/note.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'package:sqfentity/sqfentity.dart';
+import 'package:sqfentity_gen/sqfentity_gen.dart';
+part "note.g.dart";
+
+const tagTable = SqfEntityTable(
+  tableName: "tag",
+  primaryKeyName: "id",
+  primaryKeyType: PrimaryKeyType.integer_auto_incremental,
+  useSoftDeleting: true,
+  fields: [
+    SqfEntityField("tag", DbType.text, isNotNull: true),
+  ],
+);
+
+const notesTable = SqfEntityTable(
+  tableName: "note",
+  primaryKeyName: "id",
+  primaryKeyType: PrimaryKeyType.integer_auto_incremental,
+  useSoftDeleting: true,
+  fields: [
+    SqfEntityField("title", DbType.text, isNotNull: true),
+    SqfEntityField("body", DbType.text),
+    SqfEntityFieldRelationship(
+      parentTable: tagTable,
+      fieldName: "tag",
+    ),
+    SqfEntityField("createdAt", DbType.datetime),
+    SqfEntityField("updatedAt", DbType.datetime),
+  ],
+);
+
+@SqfEntityBuilder(myDbModel)
+const myDbModel = SqfEntityModel(
+  modelName: "Notes",
+  databaseName: "notes.db",
+  databaseTables: [notesTable, tagTable],
+);
