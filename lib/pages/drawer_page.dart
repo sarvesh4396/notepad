38c27acc9a83157d019bcdf07c9aa65fe933cdb6
sarvesh4396 <heysarvesh@pm.me>
2022-12-27 23:50:58 +0530

    add drawer page

---
+++ lib/pages/drawer_page.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:notepad/constants/app_strings.dart';
+import 'package:notepad/pages/tag_page.dart';
+import 'package:notepad/pages/trash_page.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class CustomDrawer extends StatefulWidget {
+  const CustomDrawer({super.key});
+
+  @override
+  State<CustomDrawer> createState() => _CustomDrawerState();
+}
+
+class _CustomDrawerState extends State<CustomDrawer> {
+  int selected = -1;
+
+  void changeSelected(int index) {
+    setState(
+      () {
+        selected = index;
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          const SizedBox(
+            height: 60,
+            child: Center(
+              child: Text(
+                "NotePad",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.w600,
+                  letterSpacing: 1.5,
+                ),
+              ),
+            ),
+          ),
+          const Divider(
+            thickness: 5,
+            indent: 10,
+            endIndent: 10,
+          ),
+          ListTile(
+            selected: selected == 0,
+            leading: Icon(Icons.note),
+            title: const DrawerText(text: "Notes"),
+            dense: true,
+            onTap: () {
+              changeSelected(0);
+              Navigator.of(context).pop();
+            },
+          ),
+          ListTile(
+            selected: selected == 1,
+            leading: const Icon(Icons.delete),
+            title: const DrawerText(text: "Trash"),
+            dense: true,
+            onTap: () {
+              changeSelected(1);
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const TrashPage(),
+                ),
+              );
+            },
+          ),
+          ListTile(
+            selected: selected == 2,
+            leading: const Icon(Icons.tag),
+            title: const DrawerText(text: "Tags"),
+            dense: true,
+            onTap: () {
+              changeSelected(2);
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const TagsPage(),
+                ),
+              );
+            },
+          ),
+          const Divider(
+            thickness: 3,
+            indent: 10,
+            endIndent: 10,
+          ),
+          ListTile(
+            selected: selected == 3,
+            leading: Icon(Icons.local_play),
+            title: const DrawerText(text: "Rate this App"),
+            dense: true,
+            onTap: () {
+              changeSelected(3);
+              launchUrl(
+                Uri.parse(AppStrings.appUrl),
+                mode: LaunchMode.externalApplication,
+              );
+            },
+          ),
+          ListTile(
+            selected: selected == 4,
+            leading: const Icon(Icons.local_mall),
+            title: const DrawerText(text: "More Apps"),
+            dense: true,
+            onTap: () {
+              changeSelected(4);
+              // launchUrl(
+              //   Uri.parse(ConstStrings.publisherUrl),
+              //   mode: LaunchMode.externalApplication,
+              // );
+            },
+          ),
+          ListTile(
+            selected: selected == 5,
+            leading: const Icon(Icons.description),
+            title: const DrawerText(text: "Privacy Policy"),
+            dense: true,
+            onTap: () {
+              changeSelected(5);
+
+              launchUrl(
+                Uri.parse(AppStrings.policyUrl),
+                mode: LaunchMode.externalApplication,
+              );
+            },
+          ),
+          const Divider(
+            thickness: 3,
+            indent: 10,
+            endIndent: 10,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class DrawerText extends StatelessWidget {
+  const DrawerText({
+    Key? key,
+    required this.text,
+  }) : super(key: key);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: const TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.w600,
+        letterSpacing: 1.5,
+      ),
+      softWrap: true,
+    );
+  }
+}
