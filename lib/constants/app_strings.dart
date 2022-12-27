c74cc85243883784e33f7c058fa2f8433df277e9
sarvesh4396 <heysarvesh@pm.me>
2022-12-27 23:36:13 +0530

    add app strings

---
+++ lib/constants/app_strings.dart
@@ -0,0 +1,12 @@
+class AppStrings {
+  static const appName = "Simple NotePad";
+  static const packageNameID = "notepad";
+  static const packageName = "com.heycoder.$packageNameID";
+  static const playStoreUrl = "https://play.google.com/store";
+  static const publisherName = "Hey Coder";
+  static const authorName = "Sarvesh Kumar Dwivedi";
+  static const appUrl = "$playStoreUrl/apps/details?id=$packageName";
+  static const publisherUrl =
+      "$playStoreUrl/search?q=pub:$publisherName&c=apps";
+  static const policyUrl = "https://heycoder.deta.dev/Simple Notepad/policy";
+}
